7623329a2da36d0cc2869cc508ec57e365274ebc
DhaWork <dhadhiyas01@gmail.com>
2024-10-08 00:13:34 +0700

    update slicing [layouts>index] (footer slicing) <3 step>

---
+++ resources/views/includes/landing/script.blade.php
@@ -0,0 +1,8 @@
+<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+
+<!-- Plugins for this template -->
+<script src="{{ asset('assets/js/jquery-plugin-collection.js') }}"></script>
+
+<!-- Custom script for this template -->
+<script src="{{ asset('assets/js/script.js') }}"></script>
---
+++ resources/views/includes/landing/style.blade.php
@@ -0,0 +1,19 @@
+<link href="{{ asset('assets/css/themify-icons.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/flaticon.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/animate.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/owl.carousel.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/owl.theme.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/slick.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/slick-theme.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/swiper.min.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/odometer-theme-default.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/owl.transitions.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/jquery.fancybox.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
